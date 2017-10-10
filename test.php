25130c8e66ef3f22675dccd8610dc7874f19e67a
delasalle-sio-caplet-a <delasalle.sio.caplet.a@gmail.com>
2017-10-10 14:32:13 +0200

    comment

--- test.php
+++ test.php
@@ -1,11 +1,12 @@
 <?php
 // création du fichier par arthur le 26/09/2017
 // ajout d'une ligne par arthur le 26/09/2017 16:26
+/*
 coucou
 // ajout d'une nouvelle ligne par arthur le 26/09/2017 17:07
 salut
 jjj
 lol
-salut les amies lol 
+salut les amies lol */
 ?>
 
